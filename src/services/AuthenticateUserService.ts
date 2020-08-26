869512506a1cb8d2aba0d76ca75d21a0439d2dbd
AntonioNtV <antoniontv18@gmail.com>
2020-08-26 12:30:35 -0300

    Gerando o token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -27,9 +29,12 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination.');
         }
 
-        // User Autenticado
+        const token = sign({}, 'secret', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
 
-        return { user };
+        return { user, token };
     }
 }
 
