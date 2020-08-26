fe4ca52e2df67529342cc379dc15744673680747
AntonioNtV <antoniontv18@gmail.com>
2020-08-26 14:19:37 -0300

    Servindo arquivo de imagens de forma estatica

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 // Iniciando banco de dados
 import './database';
 
 const app = express();
+app.use('/files', express.static(uploadConfig.directory));
 app.use(express.json());
 app.use(routes);
 
