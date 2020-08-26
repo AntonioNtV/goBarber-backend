ed6d3cf28ae1e43c20b2cde878b567ebe42c56f1
AntonioNtV <antoniontv18@gmail.com>
2020-08-26 14:16:04 -0300

    Adicioando caminho da pasta temp, no arquivo de confiuraçoes de upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,7 +2,10 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
+const tmpFolter = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+    directory: tmpFolter,
     storage: multer.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'tmp'),
         filename: (request, file, callback) => {
