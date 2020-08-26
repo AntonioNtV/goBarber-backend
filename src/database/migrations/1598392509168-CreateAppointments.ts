9c69480e1906ea34690af6035aa58885514a8da0
AntonioNtV <antoniontv18@gmail.com>
2020-08-26 10:29:34 -0300

    Minor fix

--- src/database/migrations/1598392509168-CreateAppointments.ts
+++ src/database/migrations/1598392509168-CreateAppointments.ts
@@ -19,7 +19,7 @@ export default class CreateAppointments1598392509168
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
